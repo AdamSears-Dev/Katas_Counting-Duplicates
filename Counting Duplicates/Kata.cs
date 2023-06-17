b5dad47b3458c32be0a29da9e313beda00c08f98
Adam Sears <adam_sears031@hotmail.com>
2023-06-17 17:34:33 -0400

    Initial commit

---
+++ Counting Duplicates/Kata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int DuplicateCount(string str)
+    {
+        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+        // Convert the string to lowercase for case-insensitive comparison
+        string lowerCaseStr = str.ToLower();
+
+        foreach (char c in lowerCaseStr)
+        {
+            if (charCounts.ContainsKey(c))
+            {
+                charCounts[c]++;
+            }
+            else
+            {
+                charCounts[c] = 1;
+            }
+        }
+
+        int duplicateCount = 0;
+        foreach (int count in charCounts.Values)
+        {
+            if (count > 1)
+            {
+                duplicateCount++;
+            }
+        }
+
+        return duplicateCount;
+    }
+}
+
---
+++ Counting Duplicates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+/* I used a Dictionary<char, int> to store the count of each 
+character in the string.It converts the input string to lowercase to perform 
+a case-insensitive comparison. Then, it iterates over each character in the 
+lowercase string, updating the count in the dictionary accordingly. After 
+counting the occurrences of each character, the code loops through the values in the 
+dictionary and increments the duplicateCount variable if the count is greater
+than 1. It returns the total count of characters that occur more than once!
+When you call the DuplicateCount method with a string, it will return the count
+of distinct case-insensitive alphabetic characters and numeric digits that occur 
+more than once in the input string. */
+
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = "aabBcde"; // Replace with your desired input string
+
+        int result = Kata.DuplicateCount(input);
+        Console.WriteLine($"Duplicate count: {result}");
+    }
+}
